5b21be55d016b55b59ac4b8463173fa9b28a86fa
WaterFoxPrud <nikita.prudnichenko@mail.ru>
2026-05-27 14:56:55 +0800

    Add files via upload

---
+++ ComputerGraphicProject.cpp
@@ -0,0 +1,325 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+// Подключение GLM
+#include "glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+// Подключение графического API
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h> 
+
+// Подключение библиотек ASSIMP
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+const unsigned int SCR_WIDTH = 1024;
+const unsigned int SCR_HEIGHT = 768;
+
+// Глобальные переменные для FPS-камеры
+glm::vec3 cameraPos = glm::vec3(0.0f, 2.0f, 6.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+// Прототипы функций
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+};
+
+class Mesh {
+public:
+    std::vector<Vertex>       vertices;
+    std::vector<unsigned int> indices;
+    unsigned int VAO;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices) {
+        this->vertices = vertices;
+        this->indices = indices;
+        setupMesh();
+    }
+
+    void Draw(unsigned int shaderProgram) {
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+    }
+
+private:
+    unsigned int VBO, EBO;
+
+    void setupMesh() {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+        // Атрибут координат (location = 0)
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+        // Атрибут нормалей (location = 1)
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+        glBindVertexArray(0);
+    }
+};
+
+class Model {
+public:
+    std::vector<Mesh> meshes;
+    std::string directory;
+
+    Model(std::string const& path) {
+        loadModel(path);
+    }
+
+    void Draw(unsigned int shaderProgram) {
+        for (unsigned int i = 0; i < meshes.size(); i++) {
+            meshes[i].Draw(shaderProgram);
+        }
+    }
+
+private:
+    void loadModel(std::string const& path) {
+        Assimp::Importer importer;
+        const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+            return;
+        }
+        directory = path.substr(0, path.find_last_of('/'));
+
+        processNode(scene->mRootNode, scene);
+    }
+
+    void processNode(aiNode* node, const aiScene* scene) {
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            meshes.push_back(processMesh(mesh, scene));
+        }
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            processNode(node->mChildren[i], scene);
+        }
+    }
+
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene) {
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+
+        for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            Vertex vertex;
+            glm::vec3 vector;
+
+            vector.x = mesh->mVertices[i].x;
+            vector.y = mesh->mVertices[i].y;
+            vector.z = mesh->mVertices[i].z;
+            vertex.Position = vector;
+
+            if (mesh->HasNormals()) {
+                vector.x = mesh->mNormals[i].x;
+                vector.y = mesh->mNormals[i].y;
+                vector.z = mesh->mNormals[i].z;
+                vertex.Normal = vector;
+            }
+            vertices.push_back(vertex);
+        }
+
+        for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+            aiFace face = mesh->mFaces[i];
+            for (unsigned int j = 0; j < face.mNumIndices; j++) {
+                indices.push_back(face.mIndices[j]);
+            }
+        }
+
+        return Mesh(vertices, indices);
+    }
+};
+
+int main()
+{
+    if (!glfwInit()) {
+        fprintf(stderr, "Error: couldn't start GLFW3.\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* MyWindow = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MyWindow", NULL, NULL);
+    if (!MyWindow) {
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(MyWindow);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Error initializing GLAD\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwSetInputMode(MyWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(MyWindow, mouse_callback);
+
+    glEnable(GL_DEPTH_TEST);
+
+    // Вершинный шейдер под матрицы трансформации
+    const char* vert_shader =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aNormal;\n"
+        "uniform mat4 model;\n"
+        "uniform mat4 view;\n"
+        "uniform mat4 projection;\n"
+        "void main() {\n"
+        "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+        "}\0";
+
+    // Фрагментный шейдер с flat закраской
+    const char* frag_shader =
+        "#version 330 core\n"
+        "uniform vec3 lightColor;\n"
+        "out vec4 frag_colour;\n"
+        "void main() {\n"
+        "   frag_colour = vec4(lightColor, 1.0);\n"
+        "}\0";
+
+    GLuint vert_shaders = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert_shaders, 1, &vert_shader, NULL);
+    glCompileShader(vert_shaders);
+
+    GLuint frag_shaders = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shaders, 1, &frag_shader, NULL);
+    glCompileShader(frag_shaders);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, frag_shaders);
+    glAttachShader(shader_program, vert_shaders);
+    glLinkProgram(shader_program);
+    glDeleteShader(vert_shaders);
+    glDeleteShader(frag_shaders);
+
+    GLint lightColorLoc = glGetUniformLocation(shader_program, "lightColor");
+    GLint modelLoc = glGetUniformLocation(shader_program, "model");
+    GLint viewLoc = glGetUniformLocation(shader_program, "view");
+    GLint projLoc = glGetUniformLocation(shader_program, "projection");
+
+    // Загрузка модели из папки с проектом.
+    Model ourModel("Lab3ModelVar27.obj");
+
+    while (!glfwWindowShouldClose(MyWindow))
+    {
+        glfwPollEvents();
+        processInput(MyWindow);
+
+        // Зеленый цвет фона
+        glClearColor(0.3f, 1.0f, 0.4f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+
+        // Цвет закраски модели
+        glUniform3f(lightColorLoc, 0.44f, 0.85f, 0.89f);
+
+        float aspect = (float)SCR_WIDTH / (float)SCR_HEIGHT;
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 100.0f);
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        glm::mat4 model = glm::mat4(1.0f);
+        // Небольшое масштабирование
+        model = glm::scale(model, glm::vec3(0.5f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        ourModel.Draw(shader_program);
+
+        glfwSwapBuffers(MyWindow);
+    }
+
+    glDeleteProgram(shader_program);
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    const float cameraSpeed = 0.05f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+    cameraFront = glm::normalize(front);
+}
